71dc9cae7379796afca91b478dff535020d85974
Josh Zojonc <josh.zojonc@oregonstate.edu>
2021-02-03 15:31:49 -0800

    Parsing the pdf

--- test.py
+++ test.py
@@ -1,22 +1,35 @@
 from pprint import pprint
-import PyPDF2 
-
-pdfFileObj = open('VRF-info-ALL.pdf', 'rb') 
-
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-
-pageObj = pdfReader.getPage(10) 
-
-pagetxt = (pageObj.extractText()).split('\n')
-vrfname = print(pagetxt[1])
-getnets = False
-for line in pagetxt:
-    if(getnets):
-        pprint(line)
-    if(line == 'VRF Superblock network(s):'):
-        getnets = True
-    if(getnets and line == ''):
-        getnets = False
+import PyPDF2
+from netaddr import IPNetwork, IPAddress
+import csv
 
 
-pdfFileObj.close() 
+
+#the file below needs to be the same directory as this file.
+pdfFileObj = open('VRF-info-ALL.pdf', 'rb')
+
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+pages = pdfReader.getNumPages()
+#to hold all of our cidrs
+cidrs = {}
+
+#cause you never not start at 0 for an incrementer
+i = 0
+#iterate over the PDF and create a "dictionary" mapping subnet to vrfname
+for i in range(pages):
+    pageObj = pdfReader.getPage(i)
+    pagetxt = (pageObj.extractText()).split('\n')
+    vrfname = pagetxt[1]
+    getnets = False
+    for line in pagetxt:
+        if(getnets and line.startswith('Page')):
+            getnets = False
+        if(getnets and not line.startswith('-')):
+            cidr = (line.split(' '))[2]
+            cidrs[cidr] = vrfname
+        if(line == 'Router SVIs Found:'):
+            getnets = True
+
+
+pdfFileObj.close()
