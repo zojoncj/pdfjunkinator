54983e80d0e54cdfad1078619b08fc0ac26a49c2
Josh Zojonc <josh.zojonc@oregonstate.edu>
2021-02-03 08:58:18 -0800

    Work

--- test.py
+++ test.py
@@ -5,9 +5,18 @@ pdfFileObj = open('VRF-info-ALL.pdf', 'rb')
 
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
 
-
 pageObj = pdfReader.getPage(10) 
 
-pprint(pageObj.extractText()) 
+pagetxt = (pageObj.extractText()).split('\n')
+vrfname = print(pagetxt[1])
+getnets = False
+for line in pagetxt:
+    if(getnets):
+        pprint(line)
+    if(line == 'VRF Superblock network(s):'):
+        getnets = True
+    if(getnets and line == ''):
+        getnets = False
+
 
 pdfFileObj.close() 
