b024e34150c690741c5c199a62dc94f78ff88e9f
Josh Zojonc <josh.zojonc@oregonstate.edu>
2021-02-04 10:19:19 -0800

    more work

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 from pprint import pprint
 import PyPDF2
-from netaddr import IPNetwork, IPAddress
+import ipaddress
 import csv
 
+cidrs = {}
 
+def iptovrf(ip):
+    retval = 'NotFound'
+    for cidr in cidrs:
+        if ipaddress.ip_address(ip) in ipaddress.ip_network(cidr):
+            retval= cidrs[cidr]
+    return retval
 
 #the file below needs to be the same directory as this file.
 pdfFileObj = open('VRF-info-ALL.pdf', 'rb')
@@ -12,7 +19,6 @@ pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 pages = pdfReader.getNumPages()
 #to hold all of our cidrs
-cidrs = {}
 
 #cause you never not start at 0 for an incrementer
 i = 0
@@ -33,3 +39,26 @@ for i in range(pages):
 
 
 pdfFileObj.close()
+ccsvo = open('cidrs.csv','w', newline='\n')
+for k in cidrs:
+    ccsvo.write(k+','+cidrs[k]+"\n")
+ccsvo.close()
+
+
+
+
+#cause I'm lazy and don't want to find why we have a subnet that matches the empty string
+cidrs.pop('', None)
+
+csvo = open('outfile.csv','w', newline='\n')
+csvout = csv.writer(csvo,delimiter=",",quotechar='"',quoting=csv.QUOTE_MINIMAL)
+
+with open('ip_to_hostname.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    headers = next(csvreader)
+    headers.append('VRF')
+    csvout.writerow(headers)
+    for row in csvreader:
+        ip = row[1]
+        row.append(iptovrf(ip))
+        csvout.writerow(row)
